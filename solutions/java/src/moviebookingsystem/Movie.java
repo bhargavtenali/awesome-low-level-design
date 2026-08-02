ec043e864c7af5fddde859a627cd4675c8876289
Bhargav Tenali <bhargavtenali@gmail.com>
2026-08-02 19:41:50 +0530

    Added Movie Booking System from hellointerview

---
+++ solutions/java/src/moviebookingsystem/Movie.java
@@ -0,0 +1,18 @@
+package moviebookingsystem;
+public class Movie {
+    private final String id;
+    private final String title;
+
+    public Movie(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
